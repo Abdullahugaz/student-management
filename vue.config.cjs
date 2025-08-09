0630a422633830a22aa198cc0c16ebfa028afb80
Abdullahugaz <abdullahugaz@gmail.com>
2025-08-09 17:51:45 +0300

    fix vercel error

--- vue.config.cjs
+++ vue.config.cjs
@@ -1,8 +1,11 @@
+// vue.config.cjs
 const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  // (optional) while debugging ESLint/version issues:
+  // lintOnSave: false,
   configureWebpack: {
     resolve: {
       alias: {
